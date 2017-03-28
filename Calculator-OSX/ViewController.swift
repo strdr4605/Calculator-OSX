d2f6ef1693d4b92e2c2850462c8642d434181bcd
strdr4605 <str.dr4605@gmail.com>
2017-03-29 00:32:52 +0300

    Initial Commit

---
+++ Calculator-OSX/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Calculator-OSX
+//
+//  Created by Dragos Strainu on 29.03.2017.
+//  Copyright © 2017 Dragos Strainu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
