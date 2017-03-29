cbfb3e81f9fa5653376bf57424ac8e68112bf1f5
strdr4605 <str.dr4605@gmail.com>
2017-03-29 19:57:30 +0300

    Created Simple Calculator UI

--- Calculator-OSX/ViewController.swift
+++ Calculator-OSX/ViewController.swift
@@ -12,42 +12,93 @@ class ViewController: NSViewController {
     
     @IBOutlet weak var stack: NSTextField!
     @IBOutlet weak var lastNumber: NSTextField!
-    
+    var clearLastNumber = false;
+    var clearStack = false;
     
     
     @IBAction func numbers(_ sender: NSButton) {
-        lastNumber.stringValue = lastNumber.stringValue + String(sender.tag - 1);
+        if (clearStack) {
+            stack.stringValue = "";
+            clearStack = !clearStack;
+        }
+        if (!clearLastNumber) {
+            lastNumber.stringValue = lastNumber.stringValue + String(sender.tag - 1);
+        } else {
+            lastNumber.stringValue = String(sender.tag - 1);
+            clearLastNumber = false;
+        }
     }
     
     
     @IBAction func operation(_ sender: NSButton) {
 //        tags
+//        13 percent
 //        14 Division
 //        15 Multipliction
 //        16 Minus
 //        17 Plus
-        switch sender.tag {
-        case 14:
-            stack.stringValue = stack.stringValue + lastNumber.stringValue + "/";
-            break;
-        case 15:
-            stack.stringValue = stack.stringValue + lastNumber.stringValue + "*";
-            break;
-        case 16:
-            stack.stringValue = stack.stringValue + lastNumber.stringValue + "-";
-            break;
-        case 17:
-            stack.stringValue = stack.stringValue + lastNumber.stringValue + "+";
-            break;
-        default:
-            break;
+        if (lastNumber.stringValue != "") {
+            switch sender.tag {
+            case 13:
+                lastNumber.stringValue = String(lastNumber.doubleValue / 100);
+                clearLastNumber = true;
+                break;
+            case 14:
+                stack.stringValue = stack.stringValue + lastNumber.stringValue + "/";
+                clearLastNumber = true;
+                break;
+            case 15:
+                stack.stringValue = stack.stringValue + lastNumber.stringValue + "*";
+                clearLastNumber = true;
+                break;
+            case 16:
+                stack.stringValue = stack.stringValue + lastNumber.stringValue + "-";
+                clearLastNumber = true;
+                break;
+            case 17:
+                stack.stringValue = stack.stringValue + lastNumber.stringValue + "+";
+                clearLastNumber = true;
+                break;
+            default:
+                break;
+            }
         }
     }
+    
+    
+    @IBAction func equal(_ sender: NSButton) {
+        stack.stringValue = stack.stringValue + lastNumber.stringValue;
+        clearLastNumber = true;
+        clearStack = true;
+    }
+    
+    
+    @IBAction func delete(_ sender: NSButton) {
+        if (lastNumber.stringValue != "") {
+            lastNumber.stringValue.remove(at: lastNumber.stringValue.index(before: lastNumber.stringValue.endIndex));
+        } else {
+            stack.stringValue = "";
+        }
+    }
+    
+    
+    @IBAction func reverse(_ sender: NSButton) {
+        if (lastNumber.stringValue != "") {
+            if (lastNumber.stringValue[lastNumber.stringValue.startIndex] == "-" ) {
+                lastNumber.stringValue = String(lastNumber.stringValue.characters.dropFirst());
+            } else {
+                lastNumber.stringValue = "-" + lastNumber.stringValue;
+            }
+        }
+    }
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
     }
 
     override var representedObject: Any? {
